7a6fc38253a13a744e1a1c0af4f41d1df4533a5e
flyfmgh <55116172+flyfmgh@users.noreply.github.com>
2026-08-29 22:20:55 +0000

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-plugins { id("com.android.application") }
-
-android { namespace = "com.flycinemaai"; compileSdk = 35
-    defaultConfig { applicationId = "com.flycinemaai"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+plugins {
+    id("com.android.application") version "8.6.1" apply false
 }
